29362f15de555c433bae2f5b4e4ae60c53d83cb0
Matt Wright <mattwright3195@gmail.com>
2021-12-04 14:57:17 -0800

    update client

---
+++ client/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Title</title>
+      </Head>
+      <main className="">Index page</main>
+    </div>
+  )
+}
+
+export default Home
