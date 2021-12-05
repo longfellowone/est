42b15f431d787b7d559f7e88112409a5597bd504
Matt Wright <mattwright3195@gmail.com>
2021-12-05 12:21:59 -0800

    started server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,8 @@
+// https://github.com/LukeMathWalker/zero-to-production/blob/main/tests/api/helpers.rs
+// https://github.com/diesel-rs/diesel/blob/75c688c3b246295f6f7182e0fec1b58cc685b4ed/diesel_tests/tests/select.rs
+// http://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/second-edition/ch11-03-test-organization.html
+// https://joshleeb.com/posts/rust-integration-tests.html
+
 fn main() {
     println!("Hello, world!");
 }
