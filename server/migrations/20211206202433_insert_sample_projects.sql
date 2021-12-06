9e2abb03dfe3fd2440ffb0809af8c12ce79a8281
Matt Wright <mattwright3195@gmail.com>
2021-12-06 15:24:36 -0800

    sqlx working

---
+++ server/migrations/20211206202433_insert_sample_projects.sql
@@ -0,0 +1,8 @@
+INSERT INTO projects (id, name) VALUES
+    ('9088339b-e0fe-4603-8c10-0e806414a526','Project 1'),
+    ('f6b718c6-7060-43de-be4b-1196c5eadc5d','Project 2'),
+    ('0bb9e8b6-c7f9-4aed-8c51-0c8a83d6e830','Project 3');
+
+
+
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,8 @@
 // https://joshleeb.com/posts/rust-integration-tests.html
 
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions, PgSslMode};
+use sqlx::FromRow;
+use uuid::Uuid;
 
 #[actix_web::main]
 async fn main() {
@@ -16,12 +18,33 @@ async fn main() {
         .ssl_mode(PgSslMode::Prefer);
 
     let pg_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(10))
+        .connect_timeout(std::time::Duration::from_secs(2))
         .connect_with(pg_options)
         .await
         .unwrap();
 
     sqlx::migrate!().run(&pg_pool).await.unwrap();
 
-    println!("Hello, world!");
+    #[derive(Debug, FromRow)]
+    struct Projects {
+        id: Uuid,
+        name: String,
+    }
+
+    // let res = sqlx::query_as::<_, Projects>("SELECT nothing FROM projects")
+    //     .fetch_all(&pg_pool)
+    //     .await
+    //     .unwrap();
+
+    let res = sqlx::query_as!(Projects, "SELECT id, name FROM projects")
+        .fetch_all(&pg_pool)
+        .await
+        .unwrap();
+
+    // sqlx::query!("INSERT INTO projects (id, name) VALUES ('21d957d2-80a2-4399-97cb-938a181ad27a','project 4')")
+    //     .execute(&pg_pool)
+    //     .await
+    //     .unwrap();
+
+    println!("{:?}", res[0]);
 }
