707b9d5f9c257d4a178fc8a905155d20a2b385cc
Matt Wright <mattwright3195@gmail.com>
2021-12-17 11:40:03 -0800

    setup basic actix

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,1 +1,50 @@
+use actix_cors::Cors;
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use anyhow::Result;
+use std::net::TcpListener;
+
 mod postgres;
+
+pub fn run(listener: TcpListener) -> Result<Server> {
+    // let data = web::Data::new();
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(Cors::permissive())
+            // .app_data(data.clone())
+            .configure(routes)
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
+
+fn routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/v1")
+            .route("", web::get().to(health_check))
+        // .service(
+        //     web::scope("/syn")
+        //         .route("", web::get().to(syn::health_check))
+        //         .route("/punchin", web::post().to(syn::punchin))
+        //         .route("/punchout", web::post().to(syn::punchout))
+        //         .service(
+        //             web::scope("/employees")
+        //                 .route("", web::get().to(employees::list))
+        //                 .service(
+        //                     web::resource("/{employee}")
+        //                         .route(web::get().to(employees::get))
+        //                         .route(web::post().to(employees::update)),
+        //                 ),
+        //         ),
+        // ),
+    );
+}
+
+pub async fn health_check(_req: HttpRequest) -> impl Responder {
+    println!("health_check hit");
+
+    HttpResponse::Ok()
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,36 +1,11 @@
 #![allow(dead_code)]
-// https://github.com/LukeMathWalker/zero-to-production/blob/main/tests/api/helpers.rs
-// https://github.com/diesel-rs/diesel/blob/75c688c3b246295f6f7182e0fec1b58cc685b4ed/diesel_tests/tests/select.rs
-// http://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/second-edition/ch11-03-test-organization.html
-// https://joshleeb.com/posts/rust-integration-tests.html
 
-// use chrono::{DateTime, Utc};
+use anyhow::Result;
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions, PgSslMode};
-// use sqlx::FromRow;
-// use uuid::Uuid;
-
-// GET /articles/3
-// {
-// "title": "whatever",
-// "comments": "/comments?article=3"
-// }
-
-// query!("...").map(|row| Foo { id: row.id, bar: Bar { price: row.price } }).fetch_all(&mut conn).await?;
-
-// sqlx::query!("SELECT * ... ").map(|row| A {
-// id: row.id,
-// b: row.b.map(|b| B { b: b }),
-// }).fetch_all(&mut conn).await?;
-
-// query_as!(XRow, "....").map(X::from)
-
-// https://gitlab.com/-/snippets/2062535
-
-// what we usually do is write a flat struct with all the columns for use
-// with QueryAs and then write a method that converts that into the nested structure you actually want
+use std::net::TcpListener;
 
 #[actix_web::main]
-async fn main() {
+async fn main() -> Result<()> {
     let pg_options = PgConnectOptions::new()
         .username("postgres")
         .password("postgres")
@@ -39,62 +14,20 @@ async fn main() {
         .database("postgres")
         .ssl_mode(PgSslMode::Prefer);
 
-    let pg_pool = PgPoolOptions::new()
+    let _pg_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_with(pg_options)
         .await
         .unwrap();
 
-    // sqlx::migrate!().run(&pg_pool).await.unwrap();
+    // TODO: move to App
+    // let config = Configuration::new()?;
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
 
-    // Setup products. Then setup assemblies. Then estimates. Then projects
-    // https://www.one-tab.com/page/GH3FJHoARRe1_t48x6FyxA
+    println!("Starting server...");
 
-    #[derive(Debug)]
-    struct Estimate {
-        id: i32,
-        estimate: String,
-        // assemblies: Vec<Assembly>,
-    }
+    // TODO: let app = App::new(config) -> app.run()
+    server::run(listener)?.await?;
 
-    #[derive(Debug)]
-    struct Assembly {
-        id: i32,
-        assembly: String,
-        quantity: i32,
-    }
-
-    let estimate_id = 1;
-
-    let estimate = sqlx::query_as!(
-        Estimate,
-        r#"
-        SELECT id, estimate
-        FROM estimates 
-        WHERE id = $1
-        "#,
-        estimate_id
-    )
-    .fetch_one(&pg_pool)
-    .await
-    .unwrap();
-
-    println!("{:?}", estimate);
-
-    let assemblies = sqlx::query_as!(
-        Assembly,
-        r#"
-        SELECT a.id as "id!", a.assembly as "assembly!", ea.quantity as "quantity!"
-        FROM assemblies a 
-        INNER JOIN estimate_assemblies ea on a.id = ea.assembly_id 
---         WHERE ea.estimate_id = $1
-        "# // estimate_id
-    )
-    .fetch_all(&pg_pool)
-    .await
-    .unwrap();
-
-    for assembly in assemblies {
-        println!("{:?}", assembly)
-    }
+    Ok(())
 }
